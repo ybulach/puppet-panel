3e3ab6689131e1fb8bfb26ba7ce372f70340c172
Yamine BULACH <ybulach@teclib.com>
2017-03-08 15:30:25 +0100

    Quickly fixed menu in responsive mode

--- panel/static/scripts/controllers/header.js
+++ panel/static/scripts/controllers/header.js
@@ -2,12 +2,20 @@
 
 angular.module('puppetPanel')
 .controller('HeaderCtrl', ['$scope', '$location', 'ApiService', function($scope, $location, ApiService) {
+  // Default menus state
+  $scope.isCollapsed = true;
+
   // Get user informations
   $scope.loggedIn = ApiService.loggedIn;
   $scope.loggedUser = ApiService.loggedUser;
 
   // Check for current page
   $scope.isCurrent = function(url) {
-    return $location.path() == url;
+    return ($location.path() == url) || $location.path().startsWith(url + '/');
   };
+
+  // Hide menu when changing URL (i.e. on menu click)
+  $scope.$on('$locationChangeStart', function() {
+    $scope.isCollapsed = true;
+  });
 }]);
