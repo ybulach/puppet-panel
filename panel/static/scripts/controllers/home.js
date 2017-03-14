5cb11bebc65a8f90f14ade9bd99627deabbe1646
Yamine BULACH <ybulach@teclib.com>
2017-03-14 09:56:21 +0100

    Added autorefresh on dashboard and nodes listing pages

--- panel/static/scripts/controllers/home.js
+++ panel/static/scripts/controllers/home.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('puppetPanel')
-.controller('HomeCtrl', ['$scope', '$location', '$http', '$filter', 'ApiService', function($scope, $location, $http, $filter, ApiService) {
+.controller('HomeCtrl', ['$scope', '$location', '$http', '$filter', '$interval', 'ApiService', function($scope, $location, $http, $filter, $interval, ApiService) {
   if(!ApiService.loggedIn()) {
     $location.path('/login');
     return;
@@ -10,16 +10,30 @@ angular.module('puppetPanel')
   $scope.nodes = {error: '', total: 0, data: {unchanged: [], changed: [], failed: [], unreported: [], unknown: []}};
 
   // Get the nodes
-  $http.get(ApiService.getConfig('url') + '/nodes')
-  .then(function(result) {
-    $scope.nodes.total = result.data.length;
-    $scope.nodes.data.unchanged = $filter('filter')(result.data, {'status': 'unchanged'}, true);
-    $scope.nodes.data.changed = $filter('filter')(result.data, {'status': 'changed'}, true);
-    $scope.nodes.data.failed = $filter('filter')(result.data, {'status': 'failed'}, true);
-    $scope.nodes.data.unreported = $filter('filter')(result.data, {'status': 'unreported'}, true);
-    $scope.nodes.data.unknown = $filter('filter')(result.data, {'status': null}, true);
-  }, function(reason) {
-    $scope.nodes.error = 'Error while loading nodes informations.';
+  var refresh = function() {
+    $scope.nodes.error = '';
+
+    $http.get(ApiService.getConfig('url') + '/nodes')
+    .then(function(result) {
+      $scope.nodes.total = result.data.length;
+      $scope.nodes.data.unchanged = $filter('filter')(result.data, {'status': 'unchanged'}, true);
+      $scope.nodes.data.changed = $filter('filter')(result.data, {'status': 'changed'}, true);
+      $scope.nodes.data.failed = $filter('filter')(result.data, {'status': 'failed'}, true);
+      $scope.nodes.data.unreported = $filter('filter')(result.data, {'status': 'unreported'}, true);
+      $scope.nodes.data.unknown = $filter('filter')(result.data, {'status': null}, true);
+    }, function(reason) {
+      $scope.nodes.error = 'Error while loading nodes informations.';
+    });
+  };
+  refresh();
+
+  // Autorefresh
+  var autorefresh = $interval(function() {
+    refresh();
+  }, 1000 * 15);
+
+  $scope.$on("$destroy", function() {
+    $interval.cancel(autorefresh);
   });
 
   // Show view when loaded
--- puppet/static/scripts/controllers/nodes.js
+++ puppet/static/scripts/controllers/nodes.js
@@ -1,28 +1,43 @@
 'use strict';
 
 angular.module('puppetPanel')
-.controller('NodesCtrl', ['$scope', '$location', '$uibModal', '$http', '$document', 'ApiService', 'NgTableParams', function($scope, $location, $uibModal, $http, $document, ApiService, NgTableParams) {
+.controller('NodesCtrl', ['$scope', '$location', '$uibModal', '$http', '$document', '$interval', 'ApiService', 'NgTableParams', function($scope, $location, $uibModal, $http, $document, $interval, ApiService, NgTableParams) {
   if(!ApiService.loggedIn()) {
   	$location.path('/login');
     return;
   }
 
-  $scope.nodes = {error: '', success: '', status: '', data: []};
+  $scope.nodes = {error: '', data: []};
   $scope.nodes.table = new NgTableParams({sorting: {name: "asc"}}, {});
 
   // Get the nodes
-  $http.get(ApiService.getConfig('url') + '/nodes')
-  .then(function(result) {
-    $scope.nodes.data = result.data;
-    $scope.nodes.table.settings({dataset: $scope.nodes.data});
-  }, function(reason) {
-    $scope.nodes.error = 'Error while loading nodes informations.';
+  var refresh = function() {
+    $scope.nodes.error = '';
+    var page = $scope.nodes.table.page();
+
+    $http.get(ApiService.getConfig('url') + '/nodes')
+    .then(function(result) {
+      $scope.nodes.data = result.data;
+      $scope.nodes.table.settings({dataset: $scope.nodes.data});
+      $scope.nodes.table.page(page);
+    }, function(reason) {
+      $scope.nodes.error = 'Error while loading nodes informations.';
+    });
+  };
+  refresh();
+
+  // Autorefresh
+  var autorefresh = $interval(function() {
+    refresh();
+  }, 1000 * 15);
+
+  $scope.$on("$destroy", function() {
+    $interval.cancel(autorefresh);
   });
 
   // Show view when loaded, and enable submit buttons
   $scope.$on('cfpLoadingBar:completed', function() {
     $scope.loaded = true;
-    $scope.nodes.status = '';
   });
 
   // Common modal parent
