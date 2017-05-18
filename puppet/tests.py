00d60e2f414b585992ae8197f2fe0d7372129297
Yamine BULACH <ybulach@teclib.com>
2017-05-18 14:11:54 +0200

    Added license

--- puppet/tests.py
+++ puppet/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+# This file is part of puppet-panel.
+#
+# puppet-panel is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# puppet-panel is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with puppet-panel.  If not, see <http://www.gnu.org/licenses/>.
 
-# Create your tests here.
+from django.test import TestCase
